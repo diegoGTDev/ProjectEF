85ecca4889d5592a387e57e42ae9db4c701b6618
diegodev <andreelopez334@gmail.com>
2023-08-08 19:32:15 -0600

    Migration InitialData

---
+++ Migrations/20230809013203_InitialData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProjectEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "CategoryID", "description", "name", "weight" },
+                values: new object[,]
+                {
+                    { new Guid("48370ccf-8013-41a3-9495-95ba8ea41602"), "Work tasks", "Work", 50 },
+                    { new Guid("48370ccf-8013-41a3-9495-95ba8ea416fb"), "Personal tasks", "Personal", 15 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Task",
+                columns: new[] { "TaskID", "CategoryID", "CreationDate", "Description", "IsCompleted", "TaskPriority", "Title" },
+                values: new object[] { new Guid("48370ccf-8013-41a3-9495-95ba8ea41601"), new Guid("48370ccf-8013-41a3-9495-95ba8ea416fb"), new DateTime(2023, 8, 8, 19, 32, 3, 164, DateTimeKind.Local).AddTicks(442), "Study Math for the sunday", false, 0, "Study for exam" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryID",
+                keyValue: new Guid("48370ccf-8013-41a3-9495-95ba8ea41602"));
+
+            migrationBuilder.DeleteData(
+                table: "Task",
+                keyColumn: "TaskID",
+                keyValue: new Guid("48370ccf-8013-41a3-9495-95ba8ea41601"));
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryID",
+                keyValue: new Guid("48370ccf-8013-41a3-9495-95ba8ea416fb"));
+        }
+    }
+}
--- Models/TaskContext.cs
+++ Models/TaskContext.cs
@@ -8,13 +8,19 @@ namespace ProjectEF{
         public TaskContext(DbContextOptions<TaskContext> options) : base (options) {}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder){
+            List<Category> categoriesInit = new List<Category>();
+            categoriesInit.Add(new Category() {CategoryID = Guid.Parse("48370ccf-8013-41a3-9495-95ba8ea416fb"), name = "Personal", description = "Personal tasks", weight = 15});
+            categoriesInit.Add(new Category() {CategoryID = Guid.Parse("48370ccf-8013-41a3-9495-95ba8ea41602"), name = "Work", description = "Work tasks", weight = 50});
             modelBuilder.Entity<Category>(category =>{
                 category.ToTable("Category");
                 category.HasKey(p => p.CategoryID);
                 category.Property(p => p.name).IsRequired().HasMaxLength(100);
                 category.Property(p => p.description);
                 category.Property(p => p.weight);
+                category.HasData(categoriesInit);
             });
+            List<Task> tasksInit = new List<Task>();
+            tasksInit.Add(new Task() {TaskID = Guid.Parse("48370ccf-8013-41a3-9495-95ba8ea41601"), CategoryID = Guid.Parse("48370ccf-8013-41a3-9495-95ba8ea416fb"), Title = "Study for exam", Description = "Study Math for the sunday", TaskPriority = Priority.Low, CreationDate = DateTime.Now, IsCompleted = false});
             modelBuilder.Entity<Task>(task =>{
                 task.ToTable("Task");
                 task.HasKey(p => p.TaskID);
@@ -25,6 +31,7 @@ namespace ProjectEF{
                 task.Property(p => p.CreationDate).IsRequired();
                 task.Property(p => p.IsCompleted).IsRequired();
                 task.Ignore(p => p.Resume);
+                task.HasData(tasksInit);
                
             });
         }
