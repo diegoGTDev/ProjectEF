ababc755793e1f83033e473c44ebda4f3378f077
diegodev <andreelopez334@gmail.com>
2023-08-23 18:10:36 -0600

    Post Method

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ app.MapGet("/dbconnection", async ([FromServices] TaskContext _context) =>{
     _context.Database.EnsureCreated();
     return Results.Ok("Base de datos en memoria: " + _context.Database.IsInMemory());
 });
-app.MapGet("/getTasks", async ([FromServices] TaskContext _context) =>{
+app.MapGet("/api/task", async ([FromServices] TaskContext _context) =>{
     return Results.Ok(_context.Tasks.Include(p=> p.Category).Where(p=> p.TaskPriority == Priority.Low));
 });
 app.MapGet("/getCategories", async ([FromServices] TaskContext _context) =>{
@@ -24,10 +24,47 @@ app.MapGet("/getCategories", async ([FromServices] TaskContext _context) =>{
 });
 
 //AddTask
-app.MapPost("/addTask", async ([FromServices] TaskContext _context, [FromBody] ProjectEF.Task task) =>{
-    _context.Tasks.Add(task);
+app.MapPost("/api/task", async ([FromServices] TaskContext _context, [FromBody] ProjectEF.Task task) =>
+{
+    task.TaskID = Guid.NewGuid();
+    task.CreationDate = DateTime.Now;
+    await _context.Tasks.AddAsync(task);
+    //Otra manera
+    // await _context.AddAsync(task);
     await _context.SaveChangesAsync();
-    return Results.Ok("Task added");
+
 });
+
+app.MapDelete("/api/task/{id}", async ([FromServices] TaskContext _context, [FromRoute] Guid id) =>
+{
+    var tareaActual = _context.Tasks.Find(id);
+
+    if (tareaActual != null)
+    {
+        _context.Remove(tareaActual);
+        await _context.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.NotFound("nel");
+});
+
+/*app.MapPut("/api/task", async ([FromServices] TaskContext _context, [FromRoute] ProjectEF.Task task) =>
+{
+    var taskToUpdate = await _context.Tasks.FindAsync(task.TaskID);
+    if (taskToUpdate == null)
+    {
+        return Results.NotFound("Task not found");
+    }
+    taskToUpdate.Title = task.Title;
+    taskToUpdate.Description = task.Description;
+    taskToUpdate.TaskPriority = task.TaskPriority;
+    taskToUpdate.IsCompleted = task.IsCompleted;
+    taskToUpdate.CategoryID = task.CategoryID;
+    await _context.SaveChangesAsync();
+    return Results.Ok("Task updated");
+});
+*/
 
 app.Run();
